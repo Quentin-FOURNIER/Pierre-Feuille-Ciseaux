62950ada709b631538723e95d8cf89d5e07d7838
fournieq <quentin2807@free.fr>
2022-02-05 12:10:02 +0100

    Strategie01 : Ajout de la stratégie 01 + commentaires

--- Quentin_FOURNIER_Pierre_Feuille_Ciseaux.pl
+++ Quentin_FOURNIER_Pierre_Feuille_Ciseaux.pl
@@ -14,8 +14,27 @@
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
 
 
-% % % % % % % % % % % % STRATEGIECST  % % % % % % % % % % % 
+
+
+% % % % % % % % % % % % STRATEGIECST  % % % % % % % % % % %	   
+% Stratégie constante qui donne toujours la pierre.	  
 
 strategieCst(_N,_R,pierre).
 
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+
+
+
+
+% % % % % % % % % % % % STRATEGIE01 % % % % % % % % % % % %
+% Stratégie simple qui alterne le papier et les ciseaux, 
+% par exemple selon le numéro de la manche 
+% (pair=>papier, impair=>ciseaux).
+
+strategie01(N,_R, papier) :-
+    N mod 2 =:= 0.
+
+strategie01(N,_R,ciseaux) :-
+    N mod 2 =:= 1.
+
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
