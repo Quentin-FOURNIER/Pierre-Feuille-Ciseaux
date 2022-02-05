3cc49de20550ef3f4f3d4fe681f9ad1c0af12893
fournieq <quentin2807@free.fr>
2022-02-05 12:19:15 +0100

    Strategie02 : Ajout de la stratégie 02

--- Quentin_FOURNIER_Pierre_Feuille_Ciseaux.pl
+++ Quentin_FOURNIER_Pierre_Feuille_Ciseaux.pl
@@ -7,29 +7,34 @@
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
 
 
-% % % % % % % % % % % % % IMPORTS % % % % % % % % % % % % %
+%%%%%%%%%%%%%%%%%%%%%%%%% IMPORTS %%%%%%%%%%%%%%%%%%%%%%%%%
 
 :- use_module(library(dif)).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%% STRATEGIECST  %%%%%%%%%%%%%%%%%%%%%
+
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % %	   
+% Stratégie constante qui donne toujours la pierre.	  %
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
-
-
-
-
-% % % % % % % % % % % % STRATEGIECST  % % % % % % % % % % %	   
-% Stratégie constante qui donne toujours la pierre.	  
 
 strategieCst(_N,_R,pierre).
 
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 
 
-% % % % % % % % % % % % STRATEGIE01 % % % % % % % % % % % %
-% Stratégie simple qui alterne le papier et les ciseaux, 
-% par exemple selon le numéro de la manche 
-% (pair=>papier, impair=>ciseaux).
+%%%%%%%%%%%%%%%%%%%%%%% STRATEGIE01 %%%%%%%%%%%%%%%%%%%%%%%
+
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+% Stratégie simple qui alterne le papier et les ciseaux,  %
+% par exemple selon le numéro de la manche 		  %
+% (pair=>papier, impair=>ciseaux).			  %
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
 
 strategie01(N,_R, papier) :-
     N mod 2 =:= 0.
@@ -37,4 +42,24 @@ strategie01(N,_R, papier) :-
 strategie01(N,_R,ciseaux) :-
     N mod 2 =:= 1.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%% STRATEGIE02 %%%%%%%%%%%%%%%%%%%%%%%
+
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+% Stratégie qui répète un cycle de coups (si le cycle est %
+% court, c'est une stratégie qui peut_ paraitre 'simple') %
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
+
+strategie02(N,_R,papier) :-
+    N mod 3 =:= 0.
+
+strategie02(N,_R,ciseaux) :-
+    N mod 3 =:= 1.
+
+strategie02(N,_R,feuille) :-
+    N mod 3 =:= 2.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
