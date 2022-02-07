2afc847dbefb0d15a4ad99c4a1bcb0a2fc7cead6
fournieq <quentin2807@free.fr>
2022-02-07 13:43:59 +0100

    StrategieN1 : Ajout de la stratégie N1 - Ajout cas liste < 3 sur strategie31

--- Quentin_FOURNIER_Pierre_Feuille_Ciseaux.pl
+++ Quentin_FOURNIER_Pierre_Feuille_Ciseaux.pl
@@ -236,6 +236,10 @@ strategie21(_N, R, ciseaux) :-
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
 couple3(3,_L, Z1,Z2,Z3,Z1, Z2,Z3).
    
+couple3(_N,Liste, _EntryPierre,_EntryPapier,_EntryCiseaux,0, 0, 0) :-
+    length(Liste, Len),
+    Len<3.
+
 % Victoire pierre
 couple3(Iterateur, [SousListe|Liste], EntryPierre,EntryPapier,EntryCiseaux,ReturnPierre, ReturnPapier, ReturnCiseaux) :-
     Iterateur1 is Iterateur + 1,
@@ -323,6 +327,110 @@ strategie31(_N, Liste,ciseaux) :-
 % papier
 strategie31(_N, Liste,papier) :-
     couple3(0,Liste,0,0,0,Pierre,Papier,Ciseaux),
+    Papier > Ciseaux,
+    Papier > Pierre.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%% STRATEGIEN1 %%%%%%%%%%%%%%%%%%%%%%%
+
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+% stratégie pour le joueur 1 qui regarde toutes les       %
+% manches pour voir si l'un de ses coups est plus 	  %
+% favorable en moyenne					  %
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+
+couple(_K,[], Z1,Z2,Z3,Z1, Z2,Z3).
+
+% Victoire pierre
+couple(Iterateur, [SousListe|Liste], EntryPierre,EntryPapier,EntryCiseaux,ReturnPierre, ReturnPapier, ReturnCiseaux) :-
+    Iterateur1 is Iterateur + 1,
+    SousListe == [pierre, ciseaux],
+    NewPierre is EntryPierre + 2,
+    NewCiseaux is EntryCiseaux,
+    NewPapier is EntryPapier,
+    couple(Iterateur1, Liste, NewPierre,NewPapier,NewCiseaux,ReturnPierre, ReturnPapier, ReturnCiseaux).
+
+% Victoire papier
+couple(Iterateur, [SousListe|Liste], EntryPierre,EntryPapier,EntryCiseaux,ReturnPierre, ReturnPapier, ReturnCiseaux) :-
+    Iterateur1 is Iterateur + 1,
+    SousListe == [papier, pierre],
+    NewPierre is EntryPierre,
+    NewCiseaux is EntryCiseaux,
+    NewPapier is EntryPapier + 2,
+    couple(Iterateur1, Liste, NewPierre,NewPapier,NewCiseaux,ReturnPierre, ReturnPapier, ReturnCiseaux).
+
+% Victoire ciseaux
+couple(Iterateur, [SousListe|Liste], EntryPierre,EntryPapier,EntryCiseaux,ReturnPierre, ReturnPapier, ReturnCiseaux) :-
+    Iterateur1 is Iterateur + 1,
+    SousListe == [ciseaux, papier],
+    NewPierre is EntryPierre,
+    NewCiseaux is EntryCiseaux + 2,
+    NewPapier is EntryPapier,
+    couple(Iterateur1, Liste, NewPierre,NewPapier,NewCiseaux,ReturnPierre, ReturnPapier, ReturnCiseaux).
+
+% Egalité pierre
+couple(Iterateur, [SousListe|Liste], EntryPierre,EntryPapier,EntryCiseaux,ReturnPierre, ReturnPapier, ReturnCiseaux) :-
+    Iterateur1 is Iterateur + 1,
+    SousListe == [pierre, pierre],
+    NewPierre is EntryPierre + 1,
+    NewCiseaux is EntryCiseaux,
+    NewPapier is EntryPapier,
+    couple(Iterateur1, Liste, NewPierre,NewPapier,NewCiseaux,ReturnPierre, ReturnPapier, ReturnCiseaux).
+
+% Egalité papier
+couple(Iterateur, [SousListe|Liste], EntryPierre,EntryPapier,EntryCiseaux,ReturnPierre, ReturnPapier, ReturnCiseaux) :-
+    Iterateur1 is Iterateur + 1,
+    SousListe == [papier, papier],
+    NewPierre is EntryPierre,
+    NewCiseaux is EntryCiseaux,
+    NewPapier is EntryPapier + 1,
+    couple(Iterateur1, Liste, NewPierre,NewPapier,NewCiseaux,ReturnPierre, ReturnPapier, ReturnCiseaux).
+
+% Egalité ciseaux
+couple(Iterateur, [SousListe|Liste], EntryPierre,EntryPapier,EntryCiseaux,ReturnPierre, ReturnPapier, ReturnCiseaux) :-
+    Iterateur1 is Iterateur + 1,
+    SousListe == [ciseaux, ciseaux],
+    NewPierre is EntryPierre,
+    NewCiseaux is EntryCiseaux + 1,
+    NewPapier is EntryPapier,
+    couple(Iterateur1, Liste, NewPierre,NewPapier,NewCiseaux,ReturnPierre, ReturnPapier, ReturnCiseaux).
+
+% Défaite ciseaux
+couple(Iterateur, [SousListe|Liste], EntryPierre,EntryPapier,EntryCiseaux,ReturnPierre, ReturnPapier, ReturnCiseaux) :-
+    Iterateur1 is Iterateur + 1,
+    SousListe == [ciseaux, pierre],
+    NewPierre is EntryPierre,
+    NewCiseaux is EntryCiseaux,
+    NewPapier is EntryPapier,
+    couple(Iterateur1, Liste, NewPierre,NewPapier,NewCiseaux,ReturnPierre, ReturnPapier, ReturnCiseaux).
+
+% Défaite pierre
+couple(Iterateur, [SousListe|Liste], EntryPierre,EntryPapier,EntryCiseaux,ReturnPierre, ReturnPapier, ReturnCiseaux) :-
+    Iterateur1 is Iterateur + 1,
+    SousListe == [pierre, papier],
+    NewPierre is EntryPierre,
+    NewCiseaux is EntryCiseaux,
+    NewPapier is EntryPapier,
+    couple(Iterateur1, Liste, NewPierre,NewPapier,NewCiseaux,ReturnPierre, ReturnPapier, ReturnCiseaux).
+
+% pierre
+strategieN1(_N, Liste,pierre) :-
+    couple(0,Liste,0,0,0,Pierre,Papier,Ciseaux),
+    Pierre >= Papier,
+    Pierre >= Ciseaux.
+
+% ciseaux
+strategieN1(_N, Liste,ciseaux) :-
+    couple(0,Liste,0,0,0,Pierre,Papier,Ciseaux),
+    Ciseaux >= Papier,
+    Ciseaux > Pierre.
+
+% papier
+strategieN1(_N, Liste,papier) :-
+    couple(0,Liste,0,0,0,Pierre,Papier,Ciseaux),
     Papier > Ciseaux,
     Papier > Pierre.
 
